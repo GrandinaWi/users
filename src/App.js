0ca8ac344a4635e9f0c6a77e7d23ef997e49d19d
vadgab9@gmail.com <vadgab9@gmail.com>
2024-03-03 23:13:17 +0500

    Добави логику

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,10 +6,54 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [search,setSearch]=useState('')
+    const [invite,setInvite]=useState([])
+    const [count,setCount]=useState(false);
+
+    useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((res)=>res.json())
+                .then((json)=>{
+               setUsers(json.data);
+        })
+            .catch((err)=>{
+            console.warn(err);
+                   alert('Ошибка при загрузке пользователей');
+                })
+            .finally(()=>setLoading(false));
+    }, [count]);
+    const onChangeSearch = (event)=>{
+        setSearch(event.target.value);
+    }
+    const onClickInvite = (id)=>{
+        if (invite.includes(id)){
+            setInvite(prev=>prev.filter(_id=>_id!==id));
+        }else{
+            setInvite((prev)=>[...prev,id])
+        }
+    }
+
+    const sendUsers=()=>{
+        setCount(true);
+    }
+    function back(){
+        setUsers([]);
+        setSearch('');
+        setLoading(true)
+        setInvite([])
+        setCount(false);
+    }
+    return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+        {
+            count ? (<Success back={back} count={invite.length} /> )
+                : (<Users sendUsers={sendUsers} onClickInvite={onClickInvite} invite={invite} onChangeSearch={onChangeSearch} search={search} items={users} isLoading={isLoading} />)
+        }
+
+
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ items, isLoading, search, onChangeSearch, invite,onClickInvite,sendUsers }) => {
+    console.log(search)
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input value={search} onChange={onChangeSearch} type="text" placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +20,20 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+            {
+                items.filter((obj)=>{
+                    const fullname=(obj.first_name+' '+obj.last_name).toLowerCase();
+                    return fullname.includes(search.toLowerCase()) || obj.email.includes(search.toLowerCase());
+                }).map((obj)=>(
+                    <User onClickInvite={onClickInvite} isInvited={invite.includes(obj.id)} key={obj.id} {...obj} />
+                ))
+            }
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+        {
+            invite.length>0 && (<button onClick={sendUsers} className="send-invite-btn">Отправить приглашение</button>)
+        }
+
     </>
   );
 };
